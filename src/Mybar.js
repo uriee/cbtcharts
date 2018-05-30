8400672f44baa4aef5d791a50c3c705704bf1671
Uri Even Ezra <uevenezra@gmail.com>
2018-05-30 07:53:41 +0300

    fixed the 0 in the barcharts

--- src/Mybar.js
+++ src/Mybar.js
@@ -2,6 +2,37 @@ import React from 'react'
 import {ResponsiveContainer, BarChart,  Bar, XAxis, YAxis, Legend} from 'recharts';
 import {COLORS} from './config.js';
 
+const BarLabel = (props) => {
+    const {
+        payload,
+        textAnchor,
+        fill,
+        x,
+        y,
+        width,
+        height,
+        value
+    } = props;
+    if (value[1] > 0) {
+        return (
+            <text
+                
+                textAnchor={textAnchor}
+                fill={fill}
+                x={x}
+                y={y}
+                width={width}
+                height={height}
+                >
+                {value[1]}
+            </text>
+        );
+    } else {
+        return <text></text>;
+    }
+};
+
+
 /**
  * This pure presentation class plot a stacked bar on the screen
  * 
@@ -19,7 +50,7 @@ export default React.createClass({
           <YAxis/>
           <Legend iconSize={20}/>
           {this.props.config.datakeys.map(function(dk,index) {
-              return <Bar key={index} dataKey={dk.name} stackId={dk.stack} fill={COLORS[index]} label={{ fill: COLORS[index], fontSize: 20 }} />
+              return <Bar key={index} dataKey={dk.name} stackId={dk.stack} fill={COLORS[index]} label={<BarLabel/>} / >
             })
           }
         </BarChart> 
