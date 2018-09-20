782e55694b17204a86187bfa3c27c27be0381ddd
Uri Even Ezra <uevenezra@gmail.com>
2018-09-20 13:47:47 +0300

    added efficiancy to smtcharts

--- src/SmtCharts.js
+++ src/SmtCharts.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import Smt1 from './Smt1.js';
 import Smt2 from './Smt2.js';
+import Efficiancy from './Efficiancy.js'
 
 /**
  * A function that plots a combination of the SMT graphs
@@ -11,6 +12,8 @@ export default () =>
         <div className='height90'>      
           <div className='left50'><Smt1  title='SMT Average Performance in 24hr' /></div>
            <div className='left50'><Smt2 title={'SMT Output in 24hr'} /></div>
+          <div className='gau'><Efficiancy  title='' param='smt'/></div>
+          <div className='gau2'><Efficiancy  title='' param='smt' quant='1h' /></div>           
         </div>
        )
   }
